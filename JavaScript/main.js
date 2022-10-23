53e34477b0e2a6acb133d3dad436a0c1a38147cd
LordBaljeet <lordbaljeet24@gmail.com>
2022-10-24 01:05:27 +0200

    website repo transfer

---
+++ JavaScript/main.js
@@ -0,0 +1,71 @@
+async function theoriePage() {
+
+    var links = $('.theorie');
+    for (const link of links) {
+        $(link).on('click', () => {
+            var opened = window.open("../HTML/theorie.html");
+            var lesson = link.classList[2];
+            console.log(link.classList[2]);
+            opened.sessionStorage.setItem('name', lesson);
+        });
+
+    }
+}
+
+$(function () {
+    theoriePage();
+    setDeleteBtn();
+    deletePane();
+    addLesson();
+});
+
+function deletePane() {
+    const deleteBtns = $('.delete');
+    for (const deleteBtn of deleteBtns) {
+        $(deleteBtn).on('click', () => {
+            const lesson = $(deleteBtn).parent().parent();
+            $(lesson).remove();
+        });
+    }
+}
+
+function setDeleteBtn() {
+    $('.subLinks').append('<button class="delete">');
+    $('.delete').append('<i class="fa-solid fa-trash">');
+}
+
+function addLesson() {
+    $('.addLesson').on('click', () => {
+        $('.modal').css('display', 'block');
+    });
+    $('.closeModal').on('click', () => {
+        $('.modal').css('display', 'none');
+
+    });
+    $('#create').on('click', () => {
+        if ($('#lessonName').val() != "" && $('#repoName').val() != "") {
+            const name = $('#lessonName').val();
+            const repoName = $('#repoName').val();
+            const theo = $('#theo_check').is(':checked');
+            const pra = $('#pra_check').is(':checked');
+
+            const mainDiv = '<div class="lesson class">';
+
+            $('.container').append(mainDiv);
+            $('.lesson').last().append(`<div class="classTitle">${name}</div>`);
+            $('.lesson').last().append(`<div class="classImage"></div>`);
+            const subDiv = `<div class="subLinks">`;
+            $('.lesson').last().append(subDiv);
+            if (theo) {
+                $('.subLinks').last().append(`<a href="#" class="sublink theorie ${repoName}">theorie</a>`)
+            }
+            if (pra) {
+                $('.subLinks').last().append(`<a href="#" class="sublink pratique ${repoName}">Pratique</a>`)
+            }
+            $('.subLinks').last().append('<button class="delete">');
+            $('.delete').last().append('<i class="fa-solid fa-trash">');
+            deletePane();
+            theoriePage();
+        }
+    });
+}
---
+++ JavaScript/theorie.js
@@ -0,0 +1,126 @@
+import { Octokit } from "https://cdn.skypack.dev/octokit";
+var sha = "";
+var cma = "hello";
+
+async function getFile(lesson) {
+
+    const Token = "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";
+    const octokit = new Octokit({
+        auth: Token
+    });
+
+    try {
+        const result = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+            owner: "LordBaljeet",
+            repo: "Notes",
+            path: `${lesson}/Theorie.md`
+        });
+        sha = result.data.sha;
+        sessionStorage.setItem('fileExists', result.data.content != null ? true : false);
+        return result.data.content;
+    } catch {
+        console.log("No such repository");
+        sessionStorage.setItem('fileExists', false);
+        return null;
+    }
+}
+
+function newMd(content) {
+    var md = editormd("editor", {
+        path: `../node_modules/editor.md/lib/`,
+        theme: "dark",
+        editorTheme: "blackboard",
+        previewTheme: "dark",
+        tocm: true,
+        tocDropdown: true,
+        markdown: content != null ? content : "Such Emptyness...",
+        codeFold: true,
+        emoji: true,
+        saveHTMLToTextarea: true,
+        onload() {
+            sessionStorage.setItem('content', this.cm.getValue());
+            this.toolbarHandlers.fullscreen.call(md);
+            this.toolbarHandlers.preview.call(md);
+            this.fullscreen;
+            cma = this.cm;
+            const btn = $('#saveToGit');
+            const btnLi = $('#saveLi');
+            $('.editormd-menu').append(btnLi);
+            $(btnLi).append(btn);
+        },
+        onchange() {
+            this.cm.save();
+            cma = this.cm;
+            sessionStorage.setItem('content', this.cm.getValue());
+        },
+    });
+    return md;
+}
+
+async function uploadFile(content, commit) {
+
+    const lesson = sessionStorage.getItem('name');
+    console.log(lesson);
+
+    const Token = "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";
+    const octokit = new Octokit({
+        auth: Token
+    });
+    if (sessionStorage.getItem('fileExists')=="true") {
+
+        console.log("repo exists");
+        console.log("sha: ", sha);
+        console.log("content: ", content);
+
+        const result = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+            owner: "LordBaljeet",
+            repo: "Notes",
+            path: `${lesson}/Theorie.md`,
+            message: commit,
+            content: btoa(content),
+            sha: sha
+        });
+        console.log("new file " + result.data);
+    } else {
+        console.log("repo doesn't exist");
+        const result = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+            owner: "LordBaljeet",
+            repo: "Notes",
+            path: `${lesson}/Theorie.md`,
+            message: commit,
+            content: btoa(content),
+        });
+        console.log("new file " + result.data);
+    }
+}
+
+function uploadModal() {
+    $('#saveToGit').on('click', () => {
+        $('.modal').css('display', 'block');
+    });
+    $('.closeModal').on('click', () => {
+        $('.modal').css('display', 'none');
+        $('#commitMsg').val("");
+    });
+}
+
+function upload(){
+    $('#upload').on('click', async () => {
+        if($('form')[0].checkValidity()){
+        console.log("form valid!");
+        const commit = $('#commitMsg').val();
+        const content = sessionStorage.getItem('content');
+        await uploadFile(content,commit);
+    }
+});
+
+}
+$(async function () {
+    document.title = `Theorie | ${sessionStorage.getItem('name')}`
+    const contentRaw = await getFile(sessionStorage.getItem('name'));
+    const content = contentRaw == null ? null: atob(contentRaw);
+    newMd(content);
+    $('#commitMsg').val("");
+    uploadModal();
+    upload();
+});
